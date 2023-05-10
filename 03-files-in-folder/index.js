7e1c8e0b02a25af6794411084698a1dd8469a466
vladsopot@mail.ru <vladsopot@mail.ru>
2023-05-10 23:27:10 +0300

    feat: fix 03

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -2,15 +2,26 @@ const fs = require("fs");
 const path = require("path");
 const {stdin, stdout} = process;
 
-let sizes = [];
-
 console.log(`\n\n`);
 
 fs.readdir('03-files-in-folder/secret-folder', (err, data) => {
+    let size = '';
+    let name = '';
+    let extname = '';
     data.forEach((element, i) => {
-        fs.stat('03-files-in-folder/secret-folder/'+element, (err, dat) => {
-            sizes.push(dat.size);
-            console.log(`${path.basename(element).split('.').slice(0, -1).join('.')} - ${path.extname(element).slice(1)} - ${sizes[i] * 0.001} kB`);
-        });
+        if (!fs.lstatSync(`03-files-in-folder/secret-folder/${element}`).isDirectory()) {
+            fs.stat('03-files-in-folder/secret-folder/'+element, (err, dat) => {
+                size = dat.size;
+                name = path.basename(element).split('.').slice(0, -1).join('.');
+                extname = path.extname(element).slice(1);
+                logger(name, extname, size);
+            });
+        } else {
+            return;
+        }
     });
 });
+
+function logger(name, extname, size) {
+    console.log(`${name} - ${extname} - ${size * 0.001} kB`);
+}
